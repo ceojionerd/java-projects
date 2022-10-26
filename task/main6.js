89baf0969b3b5c0b376e53368ee174c1350be9a2
Emmanuel Chijioke Ndubuisi <emmaaustin509@gmail.com>
2022-10-26 19:01:59 +0100

    updated it.

---
+++ task/main6.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(name, dob, address, phone_no, ) {
+        this.name = name;
+        this.dob = dob;
+        this.address = address;
+        this.phone_no = phone_no;
+    }
+
+}
+
+let student = new Student('John', '', '', '')
+console.log(student.name)
